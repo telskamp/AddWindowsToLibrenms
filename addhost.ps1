cbaa90f19985ac60724e57602fb2d382c62f2b78
Telskamp <tomdabomb@gmail.com>
2018-07-26 13:48:34 +0200

    fixed issuem updated remote installation commands
    -fixed issue which lead to community and ip adress not properly being forwarded to remote hosts
    - all commands remote server in one invoke
    - added icmp (ping) firewall rule

--- addhost.ps1
+++ addhost.ps1
@@ -1,10 +1,10 @@
 #define paramaters
-$baseurl = "http://librenms.example.com/api/v0"
+$baseurl = "http://librenms.yourdomain.com/api/v0"
 $Headers = @{'X-Auth-Token'='yourtokenhere'}
 $deviceurl = $baseurl + "/devices"
-$community = "public"
-$hostname = "yourhost"
-$librenmsip = "10.0.0.1"
+$community = "yoursnmpcommunity"
+$hostname = "your.hostname.com"
+$librenmsip = "10.1.2.3"
 #create body for adding hosts & convert to JSON
 $body = @{"hostname"=$hostname;
 "version"="v1";
@@ -21,31 +21,28 @@ $json = @'
 #>
 
 #create session to host for installing/enabling snmp 
+write-host "creating session to $hostname"
 $session = New-PSSession -ComputerName $hostname
 
-#install snmp on remote host
+
+
+#install snmp and allow ping trough windows firewall on remote host
+write-host "installing snmp on $hostname"
 Invoke-Command -Session $session -ScriptBlock {
 Get-WindowsFeature snmp* |Install-WindowsFeature
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\ValidCommunities" /v "$using:community" /t REG_DWORD /d 4 /f | Out-Null 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers" /v 2 /t REG_SZ /d "$using:librenmsip" /f | Out-Null 
+Restart-service snmp** 
+Import-Module NetSecurity 
+New-NetFirewallRule -Name Allow_Ping -DisplayName “Allow Ping”  -Description “server reageert op ping ICMPv4” -Protocol ICMPv4 -IcmpType 8 -Enabled True -Profile Any -Action Allow
 }
 
-#configure snmp community
-Invoke-Command -Session $session -ScriptBlock {
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\ValidCommunities" /v $community /t REG_DWORD /d 4 /f | Out-Null
-}
 
-#add snmp manager (librenms server ip) to allowed snmp monitoring machines
-Invoke-Command -Session $session -ScriptBlock {
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers" /v 2 /t REG_SZ /d $librenmsip /f | Out-Null
-}
-
-#restart snmp service to apply settings
-Invoke-Command -Session $session -ScriptBlock {
-Restart-service snmp**
-}
 #close pssession
 Remove-PSSession $session
 
 # add host to libremns
-Invoke-RestMethod -Uri $deviceurl -Headers $headers -method post -body $body -ContentType "application/json" 
+write-host "adding $hostname to librenms"
+Invoke-RestMethod -Uri $deviceurl -Headers $headers -method post -body $body -ContentType "application/json" # |select -ExpandProperty devices |ogv
 
 
